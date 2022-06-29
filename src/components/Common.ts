81487f8e13532b1aca613365e400e8e3a8c55077
Berzan <berzan@tutanota.com>
2022-06-29 14:41:14 +0300

    add: more components

--- src/components/Common.ts
+++ src/components/Common.ts
@@ -1,20 +1,26 @@
 import tailt from 'tailt'
 
-const Page = tailt.div`
+export const Page = tailt.div`
     flex flex-col
     min-h-screen
     bg-white text-zinc-900
 `
 
-const Header = tailt.header`
+export const Header = tailt.header`
     sticky top-0 z-10
     flex items-center justify-between
-    h-16
-    bg-white text-zinc-900
+    h-14
+    px-4 md:px-8
+    bg-zinc-900 text-white
 `
 
-const Footer = tailt.footer`
+export const Main = tailt.main`
+    flex flex-col items-center
+    flex-1
+`
+
+export const Footer = tailt.footer`
     flex items-center justify-between
     h-40
-    bg-zinc-200 text-zinc-900
+    bg-zinc-900 text-white
 `
---
+++ src/components/Heading.ts
@@ -0,0 +1,5 @@
+import tailt from 'tailt'
+
+export const BrandH = tailt.h1`
+    font-semibold text-2xl select-none
+` 
