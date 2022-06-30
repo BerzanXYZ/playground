bb0f231ceca92e71f8bb33000b1f75453445d7c7
Berzan <berzan@tutanota.com>
2022-06-30 12:13:51 +0300

    add: price data to CryptoContextProvider

--- src/components/DataBox/index.tsx
+++ src/components/DataBox/index.tsx
@@ -3,19 +3,47 @@ import { useCryptoContext } from '../../contexts/CryptoProvider'
 
 // "DataBox" component that display some data
 export function DataBox() {
-    const { crypto, setCrypto } = useCryptoContext()
+    const { crypto, price, setCrypto } = useCryptoContext()
+
     return (
-        <Div>{crypto}</Div>
+        <Div>
+            <Label>{crypto}: {price}</Label>
+            <ButtonBox>
+                <Button onClick={() => setCrypto("BTC")}>BTC</Button>
+                <Button onClick={() => setCrypto("ETH")}>ETH</Button>
+                <Button onClick={() => setCrypto("SOL")}>SOL</Button>
+            </ButtonBox>
+        </Div>
     )
 }
 
 // "Div" for "DataBox" component
 const Div = tailt.div`
     flex flex-col text-center justify-center
-    h-20 w-60
-    p-3 rounded-xl
+    p-4 gap-y-4 rounded-xl
     bg-zinc-900 text-white
     dark:bg-white dark:text-zinc-900
     duration-200 select-none
+`
+
+// "Label" for "DataBox" component
+const Label = tailt.h2`
     font-semibold text-xl
+`
+
+// "ButtonBox" for "Button" component
+const ButtonBox = tailt.div`
+    flex justify-between gap-x-3
+`
+
+// "Button" for "DataBox" component
+const Button = tailt.button`
+    h-10 px-4 rounded-lg
+    hover:scale-95 active:scale-100
+    bg-zinc-600 text-white
+    dark:bg-zinc-300 dark:text-zinc-900 
+    hover:bg-zinc-500 active:bg-zinc-400
+    dark:hover:bg-zinc-400 dark:active:bg-zinc-500
+    duration-200
+    font-semibold
 `
--- src/contexts/CryptoProvider.tsx
+++ src/contexts/CryptoProvider.tsx
@@ -1,10 +1,11 @@
 import exp from 'constants'
-import { createContext, ReactNode, useContext, useState } from 'react'
+import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react'
 
 
 interface CryptoContextState {
     crypto: string,
-    setCrypto(name: string): void,
+    price: string,
+    setCrypto(crypto: string): void,
 }
 
 const CryptoContext = createContext<CryptoContextState>({} as CryptoContextState)
@@ -15,8 +16,21 @@ export function useCryptoContext(): CryptoContextState {
 
 export const CryptoContextProvider = ({ children }: {children: ReactNode}) => {
     const [crypto, setCrypto] = useState<string>("BTC")
+    const [price, setPrice] = useState<string>("0.00")
+    const [cryptoName, setCryptoName] = useState<string>("BTC")
+
+    useEffect(() => {
+        async function fetchPrice() {
+            const url = `https://api.binance.com/api/v3/avgPrice?symbol=${cryptoName}USDT`
+            const data = await (await fetch(url)).json() as {price: string}
+            const p = parseInt(data.price).toLocaleString()
+            setPrice(`$${p}`)
+        }
+        fetchPrice().then(() => setCrypto(cryptoName))
+    }, [cryptoName])
+    
 
     return (
-        <CryptoContext.Provider value={{ crypto, setCrypto}}>{children}</CryptoContext.Provider>
+        <CryptoContext.Provider value={{ crypto, price, setCrypto: setCryptoName}}>{children}</CryptoContext.Provider>
     )
 }
